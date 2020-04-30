ba276f670faea4b225876e6c8d1f49e44c5aff36
eloifabius <eloifabius@gmail.com>
2020-04-30 21:49:38 +0200

    Add files via upload

--- src/components/cards_component.js
+++ src/components/cards_component.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default class cards_component extends Component {
     render() {
+        const { card } = this.props;
         return (
-            <div>
-                <h1>cards_component</h1>
+            <div className="card">
+                <div className="content2" onClick={this.updatePassword}>
+                    <div className="content_image_wrapper2">
+                    </div>
+                    <div className="content_text">
+                        <h4>{ card.name }</h4>
+                        <h5>{ card.number }</h5>
+                        <h6 className="expCard">{ card.expiry }</h6>
+                    </div>
+                </div>
             </div>
         )
     }
