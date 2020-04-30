c4db95083eb397fbfa41f760e0cfea8c9610cdd6
eloifabius <eloifabius@gmail.com>
2020-04-30 15:56:11 +0200

    Add files via upload
    front

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@
 import React from 'react';//Importation de React
 import Login from './components/login_page_component';//Importation du composant 'Login'
 import './App.css';//Importation de la feuille de style
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Userpage from './components/user_page_component';
+
 
 
 //La fonction App est le point d'entré de l'application, il return le bloc principal de l'application.
 function App() {
   return (
-    //Le bloc principal qui charge les différentes composants de l'application
+    <Router>
     <div className="App">
       {/* le composant 'Login' intègre la page de connexion et d'inscription de l'application */}
-      <Userpage />
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/home" component={Userpage}/>
+        </Switch>
     </div>
+    </Router>
   );
 }
 
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react'
 import PasswordCard from '../components/password_card_component'
+import Card from '../components/cards_component'
+import Loader from './loader_component'
 
 export default class cards extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showLoading: true
+        }
+    }
+    componentDidMount() {
+        setTimeout(() => {
+           this.setState({showLoading: false}) 
+        }, 3000);
+    }
+    callParent2() {
+        //Todo
+        console.log("todo")
+    }
     render() {
-        const { passwords } = this.props;
+        const { passwords, cards } = this.props;
         return (
             <div>
-                <div className="title_span"><h4 className="title_text">Passwords</h4></div>
-                <div className="passwords_card_view">
-                    {
-                        passwords.map(password => <PasswordCard password={password} key={password.id} />)
-                    }
-                </div>
-                <div className="title_span"><h4 className="title_text">Cards</h4></div>
-                <div className="passwords_card_view">
-                    {
-                        passwords.map(password => <PasswordCard password={password} key={password.id} />)
-                    }
-                </div>
+                {this.state.showLoading && <div className="loaderWrapper"><Loader /><h3>Chargement</h3></div>}
+                {!this.state.showLoading && 
+                     <div>
+                        <div className="title_span"><h4 className="title_text">Passwords</h4></div>
+                        <div className="passwords_card_view">
+                            {
+                                passwords.map(password => <PasswordCard password={password} key={password._id} callParent2={this.callParent2}/>)
+                            }
+                        </div>
+                        <div className="title_span"><h4 className="title_text">Cards</h4></div>
+                        <div className="passwords_card_view">
+                            {
+                                cards.map(card => <Card password={card} key={card.cardId} callParent2={this.callParent2}/>)
+                            }
+                        </div>
+                     </div>  
+                }         
             </div>
         )
     }
